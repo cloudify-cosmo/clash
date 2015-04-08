f0f6c9cac7429830ae21dd31804e6fb9ded7b058
Dan Kilman <dankilman@gmail.com>
2015-04-09 01:52:59 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='workflowcmd',
+    version='0.1',
+    author='Dan Kilman',
+    author_email='dankilman@gmail.com',
+    packages=['workflowcmd'],
+    description='Turn a Cloudify local blueprint into a CLI',
+    zip_safe=False,
+    install_requires=[
+        'argh',
+        'path.py',
+        'cloudify==3.2a8'
+    ]
+)
