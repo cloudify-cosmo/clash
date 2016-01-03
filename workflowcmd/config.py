908974b48e55df59fa30d121e43d5591cc235295
Dan Kilman <dankilman@gmail.com>
2016-01-03 21:31:01 +0200

    clean some

--- workflowcmd/config.py
+++ workflowcmd/config.py
@@ -1,4 +1,3 @@
-import importlib
 import sys
 import os
 import shutil
@@ -7,29 +6,52 @@ import json as _json
 import yaml
 import argh
 from path import path
-from cloudify_cli import utils as cli_utils
-from cloudify.workflows import local
+
 from dsl_parser import functions as dsl_functions
+from cloudify.workflows import local
+from cloudify_cli import utils as cli_utils
 
 from workflowcmd import output, util
 
 
+def _global_cdev_conf_path():
+    return os.path.expanduser(os.environ.get('CDEV_CONF_PATH', '~/.cdev'))
+
+
+def _read_global_cdev_conf():
+    cdev_conf_path = _global_cdev_conf_path()
+    if not os.path.exists(cdev_conf_path):
+        return {}
+    with open(cdev_conf_path) as f:
+        return yaml.safe_load(f)
+
+
+def _update_global_cdev_conf(conf):
+    cdev_conf_path = _global_cdev_conf_path()
+    with open(cdev_conf_path, 'w') as f:
+        return yaml.safe_dump(conf, f)
+
+
 class Loader(object):
 
-    _name = 'local'
+    _name = '.local'
 
-    def __init__(self, package, config_path, storage_dir, blueprint_path):
-        package = importlib.import_module(package)
+    def __init__(self, package, config_path):
         package_path = path(package.__file__).dirname()
         config_path = package_path / config_path
+        config_dir = config_path.dirname()
         self._config = yaml.load(config_path.text())
-        self._blueprint_path = package_path / blueprint_path
+        self._blueprint_path = config_dir / self._config['blueprint_path']
         self._blueprint_dir = self._blueprint_path.dirname()
-        self._storage_dir = path(os.path.expanduser(storage_dir))
         self._parser = argh.ArghParser()
-        self._parse_commands(commands=self._config['commands'])
-        self._parser.add_commands(functions=[self._init_command,
-                                             self._outputs_command])
+
+        self._parser.add_commands(functions=[self._setup_command])
+        storage_dir = _read_global_cdev_conf().get('storage_dir')
+        if storage_dir:
+            self._storage_dir = path(storage_dir)
+            self._parse_commands(commands=self._config['commands'])
+            self._parser.add_commands(functions=[self._init_command,
+                                                 self._outputs_command])
 
     def _parse_commands(self, commands, namespace=None):
         functions = []
@@ -56,15 +78,14 @@ class Loader(object):
             command_task_config = command.get('task', {})
             task_config.update(global_task_config)
             task_config.update(command_task_config)
-            sys.path.append(self._storage_dir / 'local' / 'resources')
+            sys.path.append(self._storage_dir / self._name / 'resources')
             env = self._load_env()
 
             event_cls = command.get('event_cls',
                                     self._config.get('event_cls'))
-            output.setup_output(
-                event_cls=event_cls,
-                verbose=args.verbose,
-                env=env)
+            output.setup_output(event_cls=event_cls,
+                                verbose=args.verbose,
+                                env=env)
 
             env.execute(workflow=command['workflow'],
                         parameters=parameters,
@@ -90,12 +111,25 @@ class Loader(object):
     def _storage(self):
         return local.FileStorage(storage_dir=self._storage_dir)
 
+    @argh.named('setup')
+    def _setup_command(self, storage_dir):
+        self._storage_dir = path(storage_dir)
+        _update_global_cdev_conf({'storage_dir': storage_dir})
+        with open(self._blueprint_path) as f:
+            blueprint = yaml.safe_load(f)
+        inputs = {key: value.get('default', '_')
+                  for key, value in blueprint.get('inputs').items()}
+        inputs_path = self._storage_dir / 'inputs.yaml'
+        inputs_path.write_text(yaml.safe_dump(inputs,
+                                              default_flow_style=False))
+
     @argh.named('init')
-    def _init_command(self, inputs=None, reset=False):
+    def _init_command(self, reset=False):
         local_dir = self._storage_dir / self._name
         if local_dir.exists() and reset:
             shutil.rmtree(local_dir)
-        inputs = cli_utils.inputs_to_dict(inputs, 'inputs')
+        inputs = cli_utils.inputs_to_dict(self._storage_dir / 'inputs.yaml',
+                                          'inputs')
         sys.path.append(self._blueprint_dir)
         local.init_env(blueprint_path=self._blueprint_path,
                        inputs=inputs,
@@ -115,11 +149,8 @@ class Loader(object):
         self._parser.dispatch()
 
 
-def dispatch(package, config_path, storage_dir, blueprint_path):
-    loader = Loader(package=package,
-                    config_path=config_path,
-                    storage_dir=storage_dir,
-                    blueprint_path=blueprint_path)
+def dispatch(package, config_path):
+    loader = Loader(package=package, config_path=config_path)
     loader.dispatch()
 
 
@@ -142,6 +173,7 @@ def _function(process):
     class Function(dsl_functions.Function):
         validate = None
         evaluate = None
+        function_args = None
 
         def parse_args(self, args):
             self.function_args = args
