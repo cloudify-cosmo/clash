5b9318698154f20baacaf484fb484399b8cbcb33
Dan Kilman <dankilman@gmail.com>
2016-01-04 13:35:07 +0200

    add licenses

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
+########
+# Copyright (c) 2016 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############
+
 from setuptools import setup
 
 setup(
     name='workflowcmd',
     version='0.1',
-    author='Dan Kilman',
-    author_email='dankilman@gmail.com',
+    author='GigaSpaces',
+    author_email='cosmo-admin@gigaspaces.com',
     packages=['workflowcmd'],
     description='Turn a Cloudify local blueprint into a CLI',
     zip_safe=False,
@@ -15,7 +31,6 @@ setup(
         'path.py',
         'cloudify-plugins-common>=3.3.0,<3.4',
         'cloudify-dsl-parser>=3.3.0,<3.4',
-        'cloudify-script-plugin>=1.3.0,<1.4',
-
+        'cloudify-script-plugin>=1.3.0,<1.4'
     ]
 )
